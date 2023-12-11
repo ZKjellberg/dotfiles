d4bf87b50a6df190460a105713aa2d4be8ba3c8a
Zachary Kjellberg <zach.kjellberg@gmail.com>
2023-12-11 09:28:12 -0500

    Update smartcard.sh

--- smartcard.sh
+++ smartcard.sh
@@ -1,6 +1,6 @@
-sudo apt install opensc
+sudo apt install opensc-pkcs11 pcscd sssd libpam-sss libnss3-tools
 modutil -dbdir sql:$HOME/.pki/nssdb/ -add "cac-module" -libfile /usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so
 
-# Install DoD Certs
+# Install DoD Certs - Not required
 wget https://militarycac.com/maccerts/AllCerts.p7b
 for n in *.p7b; do certutil -d sql:$HOME/.pki/nssdb -A -t TC,CT, -n $n -i $n; done
